ab7aed2e8f16da85da7ad3c00d1a1ab4b096a7ea
Hardiksethia <hardiksethia13@gmail.com>
2024-08-09 04:40:57 +0530

    add initial linked list implementation

---
+++ src/Linkedlist.java
@@ -0,0 +1,22 @@
+// src/LinkedList.java
+public class Linkedlist {
+    Node head; // head of list
+
+    static class Node {
+        int data;
+        Node next;
+
+        Node(int d) {
+            data = d;
+            next = null;
+        }
+    }
+
+    public void printList() {
+        Node n = head;
+        while (n != null) {
+            System.out.print(n.data + " ");
+            n = n.next;
+        }
+    }
+}
